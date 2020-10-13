0e48a3d31cc572494f6f10fa93b1efb4554d2e82
hannah-developer <hannahchoi.developer@gmail.com>
2020-10-13 21:37:12 +0900

    code refactoring : Class Timezone

--- frontEnd/script/script.js
+++ frontEnd/script/script.js
@@ -1,35 +1,10 @@
 
-const currentTimezone = moment.tz.guess();
-const selectedTimezone = "Europe/London"
-console.log(currentTimezone)
-console.log(moment().tz(`${selectedTimezone}`));
+import Timezone from './Timezone.js'
+const timezoneComps = document.querySelectorAll('.timezoneComp')
 
+timezoneComps.forEach((data)=>{
+    data.innerHTML = new Timezone('Asia/Tokyo').render()
+})
 
-const city = function(){return `${currentTimezone}`.split("/").pop();}
-const now = function(){return moment().format('HH:mm')}
-const today = function(){return moment().format('ddd, DD MMM')}
-const countryName = function(){return moment.tz.zone(`${currentTimezone}`).countries()} 
-const abbrTimezone = function(){return moment.tz(`${currentTimezone}`).format("z")}
-let hourNumber = parseInt(moment().format('HH')) < 10 ? moment().format('HH').slice(-1) : moment().format('HH')
-
-const homeTime = document.querySelector('.homeTime')
-const homeCity = document.querySelector('.homeCity')
-const homeDate = document.querySelector('.homeDate')
-const homeCode = document.querySelector('.homeCode')
-const homeTimezone = document.querySelector('.abbrZone')
-const timeLineNode = document.querySelectorAll(`[data-tz="${currentTimezone}"]`)
-
-
-const timeLine = Array.from(timeLineNode);
-
-if(hourNumber){
-    let currentHour = timeLine.find(data=> data.textContent === hourNumber)
-    currentHour.classList.add('focus')
-}
-
-homeTime.innerHTML = now()
-homeCity.innerHTML = city()
-homeDate.innerHTML = today()
-homeCode.innerHTML = countryName()
-homeTimezone.innerHTML = abbrTimezone()
+console.log(moment.tz('Europe/London').format('HH:mm'))
 
--- frontEnd/script/timezone.js
+++ frontEnd/script/timezone.js
@@ -1,8 +1,11 @@
 
 class Timezone{
+    constructor(city){
+        this.currentTimezone = city
+    }
 
-    constructor(timezone){
-        this.timezone = timezone
+    getCity(){
+        return `${this.currentTimezone}`.split("/").pop();
     }
 
     getNow(){
@@ -14,13 +17,38 @@ class Timezone{
     }
 
     getCountry(){
-        return moment.tz.zone(`${currentTimezone}`).countries()
+        return moment.tz.zone(`${this.currentTimezone}`).countries()
     }
 
     getAbbr(){
-        return moment.tz(`${currentTimezone}`).format("z")
+        return moment.tz(`${this.currentTimezone}`).format("z")
     }
 
+    getHourNumber(){
+        return parseInt(moment().format('HH')) < 10 ? moment().format('HH').slice(-1) : moment().format('HH')
+    }
+
+    render(){
+        return `
+                <div class="home">
+                    <img src="images/placeholder.svg">
+                </div>
+                <div class="timezone">
+                    <div class="timezone1">
+                        <span class="cityName homeCity">${this.getCity()}</span>
+                        <span class="time homeTime">${this.getNow()}</span>
+                    </div>
+                    <div class="timezone2">
+                        <span class="countryName homeCode">${this.getCountry()}</span>
+                        <span class="abbrZone">${this.getAbbr()}</span>
+                        <span class="date homeDate">${this.getToday()}</span>
+                    </div>
+                </div>
+                <div class="modify">
+                        <img src="images/cancel.svg" width="10px">
+                </div>
+                `
+    }
 }
 
 export default Timezone
