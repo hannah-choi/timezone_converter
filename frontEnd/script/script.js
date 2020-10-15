4389b9970df47c9bdbb30dd1eb133656efc39f03
hannah-developer <hannahchoi.developer@gmail.com>
2020-10-15 23:53:22 +0900

    load data for autocomplete

--- frontEnd/script/script.js
+++ frontEnd/script/script.js
@@ -1,6 +1,6 @@
 import Timezone from './timezone.js'
 const timezoneList = document.querySelector('.timezoneList')
-
+ 
 let cityArray = [
     {city: 'Asia/Seoul'},
     {city: 'Asia/Hong_kong'},
@@ -13,6 +13,15 @@ const cityList = cityArray.map(city => new Timezone(city.city))
 const timezoneRender = cityList.map(city => city.render()).join('')
 
 timezoneList.innerHTML = timezoneRender
+
+const timezoneDb = [];
+
+fetch("./script/latest.json")
+.then(res => res.json())
+.then(data => timezoneDb.push(data))
+
+console.log(timezoneDb)
+
 
 // timezoneComps.forEach((data)=>{
 //     data.innerHTML = new Timezone('Asia/Tokyo').render()
