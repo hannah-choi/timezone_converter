baeb9126dde29ff16a9947c3e7c892929c622ba4
hannah-developer <hannahchoi.developer@gmail.com>
2020-10-31 11:24:59 +0900

    debug timezone issue

--- frontEnd/script/hour.js
+++ frontEnd/script/hour.js
@@ -16,15 +16,20 @@ class Hour{
         return moment.tz(`${this.city}`).add(1, 'days').format('DD MMM')
     }
 
+    getClass(i){
+        return `${i === 0 ? 'date':''} ${i===this.getNow()? 'focus':''}`
+    }
+
+
     getHours(){
         let hours = '';
-        console.log(this.offset)
-            for(let i = `${this.offset < 0? (24 + this.offset) : this.offset}`; i < 24; i++){
-                hours += `<span class =${i === 0 ? 'date':''} ${i===this.getNow()? 'focus':''}>${i === 0 ? this.getToday():i}</span>`
-            }
-            for(let i = 0; i < `${this.offset < 0? (24 + this.offset) : this.offset}`; i++){
-                hours += `<span class=${i === 0 ? 'date':''} ${i===this.getNow()? 'focus':''}>${i === 0 ? (this.offset < 0 ? this.getToday() : this.getTomorrow()) :i}</span>`
-            }
+        let number = this.offset < 0 ? (24 + this.offset) : this.offset
+        for(let i = number; i < 24; i++){
+            hours += `<span class = "${this.getClass(i)}">${i === 0 ? this.getToday():i}</span>`
+        }
+        for(let i = 0; i < number; i++){
+            hours += `<span class="${this.getClass(i)}">${i === 0 ? (this.offset < 0 ? this.getToday() : this.getTomorrow()) :i}</span>`
+        }
         return hours;
     }
 
