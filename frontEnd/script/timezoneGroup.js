737feee127ed9d4717dce339b1dd128eb4c31531
hannah-developer <hannahchoi.developer@gmail.com>
2020-11-11 19:53:02 +0900

    moved methods to timezoneGroup

--- frontEnd/script/timezoneGroup.js
+++ frontEnd/script/timezoneGroup.js
@@ -3,14 +3,33 @@ class TimezoneGroup{
     constructor(timezone, hour){
         this.timezone = timezone;
         this.hour = hour;
-        console.log(timezone)
-        console.log(hour)
+        this.timezoneList = document.querySelector('.timezoneList')
+        this.hoursList = document.querySelector('.hoursList')
+        this.render()
     }
-    
 
 
+    render(){
+        this.hourListRender()
+        this.timezoneListRender()
+    }
 
+    hourListRender(){
+        const temp = document.createElement('div')
+        temp.innerHTML = this.hour.render()
+        this.hoursList.appendChild(temp.children[0])
+        this.getDs()
+    }
 
+    getDs(){
+        this.hour.getDs(this.timezone.timeUpdate, this.timezoneListRender)
+    }
+
+    timezoneListRender(){
+        const temp = document.createElement('div')
+        temp.innerHTML = this.timezone.render()
+        this.timezoneList.appendChild(temp.children[0])
+    }
 
 }
 
--- frontEnd/script/timezonemanager.js
+++ frontEnd/script/timezonemanager.js
@@ -9,12 +9,10 @@ class TimezoneManager{
     constructor(){
         this.cityArray = [];
         this.hourList = [];
-        this.hoursList = document.querySelector('.hoursList')
         this.defaultTimezone = moment.tz.guess()
         this.defaultOffset = moment.tz(`${this.defaultTimezone}`).utcOffset()/60; 
         this.suggestionList = document.querySelector('.suggestionList')
         this.searchInput = document.querySelector('.searchInput')
-        this.timezoneList = document.querySelector('.timezoneList')
         this.index = null;
         this.groupList = [];
     }
@@ -32,30 +30,17 @@ class TimezoneManager{
         return difference;
     }
 
-    timezoneListRender = (timezone) => {
-        const temp = document.createElement('div')
-        temp.innerHTML = timezone.render()
-        this.timezoneList.appendChild(temp.children[0])
-    }
-
-
 
     addZone(target) {
         const cityName = target.dataset.zone.replace(' ','_');
         const zoneName = cityName.split('/')
         .map(data => data[0].toUpperCase() + data.substr(1).toLowerCase()).join('/');
+        
         const setTimezoneGroup = new TimezoneGroup(new Timezone(zoneName, this.getDifference(zoneName)), new Hour(zoneName, parseInt(this.getDifference(zoneName)), this.getGMT(cityName)))
-        this.groupList.push()
-        this.timezoneListRender(setTimezoneGroup.timezone)
-        const temp = document.createElement('div')
-        temp.innerHTML = setTimezoneGroup.hour.render()
-        this.hoursList.appendChild(temp.children[0])
-        this.groupList.map((data,i) => data.hour.getDs(data.timezone.timeUpdate, this.timezoneListRender, i))
-
-
+        this.groupList.push(setTimezoneGroup)
+        
         this.suggestionList.innerHTML = '';
         this.searchInput.value = '';
-
     }
 
 
